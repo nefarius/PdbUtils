cb15496e3eb8848f16a4fb48e748c30ca56e422f
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-07-31 14:45:09 +0200

    Added extension methods

---
+++ Extensions/MsPdbExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Kaitai;
+
+namespace Nefarius.Shared.PdbUtils.Extensions;
+
+internal static class MsPdbExtensions
+{
+    /// <summary>
+    ///     Grabs the original file name from the PDB stream.
+    /// </summary>
+    /// <returns>The PDB name or null if not found.</returns>
+    public static string? GetOriginalPdbName(this MsPdb pdb)
+    {
+        MsPdb.UModuleInfo? pdbModule = pdb.DbiStream.ModulesList.Items
+            .FirstOrDefault(info => info.Module.EcInfo.PdbFilenameIndex != 0);
+
+        if (pdbModule is null)
+        {
+            return null;
+        }
+
+        uint index = pdbModule.Module.EcInfo.PdbFilenameIndex;
+        string? pdbPathName = pdb.DbiStream.EcInfo.Strings.Strings.FirstOrDefault(s => s.CharsIndex == index)?.String;
+
+        // required to work on Linux
+        string? normalizedPath = pdbPathName?.Replace('\\', Path.DirectorySeparatorChar);
+
+        return string.IsNullOrEmpty(normalizedPath) ? null : Path.GetFileName(normalizedPath);
+    }
+}
